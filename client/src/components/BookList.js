23f45f36ab041311c05f9a6d5d744f9f060b7ca5
Meghal Modi <meghalmodi12@gmail.com>
2020-06-28 10:48:48 -0400

    Added react client

---
+++ client/src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render() {
+        return (
+            <div>
+                <ul id="bookList">
+                    <li>Book Names</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default BookList;

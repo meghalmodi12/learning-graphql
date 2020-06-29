3ee3035aaf7e0a51c1c6ac71892dc09dc3930928
Meghal Modi <meghalmodi12@gmail.com>
2020-06-28 20:01:04 -0400

    Styling up the react app

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -20,7 +20,7 @@ class BookDetails extends Component {
                 </div>
             );
         } else {
-            return <div>No book selected</div>;
+            return <div>No book selected...</div>;
         }
     }
 
